438306d78cf4d10d719ee9a53c353334f97e9337
Fedor Resnyanskiy <tubist.fedor@gmail.com>
2017-03-19 21:17:14 +0300

    added mvnsh archetype

---
+++ mvnsh-archetype/src/main/resources/archetype-resources/src/main/resources/win/one.bat
@@ -0,0 +1,5 @@
+@echo off
+echo executing one.bat...
+
+@REM "\${this}" will be replaced with full maven artifact identifier when maven resources processing. See pom.xml properties
+@mvnsh ${this}/bat/win.two
