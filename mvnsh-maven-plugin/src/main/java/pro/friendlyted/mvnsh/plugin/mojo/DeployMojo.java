7a9332ea7c7cd72be1399f20da5fe1b246cc85fb
Fedor Resnyanskiy <tubist.fedor@gmail.com>
2017-03-20 11:31:59 +0300

    https://github.com/friendlyted/mvnsh/issues/2 Fix deploy issue

--- mvnsh-maven-plugin/src/main/java/pro/friendlyted/mvnsh/plugin/mojo/DeployMojo.java
+++ mvnsh-maven-plugin/src/main/java/pro/friendlyted/mvnsh/plugin/mojo/DeployMojo.java
@@ -1,11 +1,11 @@
 package pro.friendlyted.mvnsh.plugin.mojo;
 
 import org.apache.maven.RepositoryUtils;
-import org.apache.maven.artifact.repository.ArtifactRepository;
-import org.apache.maven.plugins.annotations.Component;
+import org.apache.maven.artifact.repository.MavenArtifactRepository;
 import org.apache.maven.plugins.annotations.InstantiationStrategy;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 import pro.friendlyted.mvnsh.core.api.MsDeploy;
 import pro.friendlyted.mvnsh.core.api.MsFactory;
 import pro.friendlyted.mvnsh.core.api.MsUpload;
@@ -25,14 +25,11 @@ import pro.friendlyted.mvnsh.core.api.MsUpload;
         instantiationStrategy = InstantiationStrategy.PER_LOOKUP
 )
 public class DeployMojo extends AbstractUploadMojo {
-
-    @Component
-    private ArtifactRepository artifactRepository;
     
     @Override
     protected MsUpload createUploadProcessor() {
         final MsDeploy deploy = MsFactory.MAVEN.deploy();
-        deploy.setArtifactRepository(RepositoryUtils.toRepo(artifactRepository));
+        deploy.setArtifactRepository(RepositoryUtils.toRepo(project.getDistributionManagementArtifactRepository()));
         return deploy;
     }
 
