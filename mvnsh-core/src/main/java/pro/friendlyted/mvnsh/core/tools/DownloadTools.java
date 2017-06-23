fffb768b3d4723746aac4119618a3c3a076d701e
Fedor Resnyanskiy <fa.resnyanskiy@jet.msk.su>
2017-06-23 10:35:18 +0300

    Added version check (for snapshots)

--- mvnsh-core/src/main/java/pro/friendlyted/mvnsh/core/tools/DownloadTools.java
+++ mvnsh-core/src/main/java/pro/friendlyted/mvnsh/core/tools/DownloadTools.java
@@ -4,10 +4,13 @@ import java.util.List;
 import org.eclipse.aether.RepositorySystem;
 import org.eclipse.aether.RepositorySystemSession;
 import org.eclipse.aether.artifact.Artifact;
+import org.eclipse.aether.artifact.DefaultArtifact;
 import org.eclipse.aether.repository.RemoteRepository;
 import org.eclipse.aether.resolution.ArtifactRequest;
 import org.eclipse.aether.resolution.ArtifactResolutionException;
 import org.eclipse.aether.resolution.ArtifactResult;
+import org.eclipse.aether.resolution.VersionRequest;
+import org.eclipse.aether.resolution.VersionResolutionException;
 
 /**
  *
@@ -17,22 +20,53 @@ public class DownloadTools {
 
     private DownloadTools() {
     }
-    
+
     public static Artifact getRemoteArtifact(
-            final Artifact localArtifact,
+            final Artifact artifact,
             List<RemoteRepository> remoteRepos,
             RepositorySystem repoSystem,
             RepositorySystemSession repoSession
     ) {
-        final ArtifactRequest request = new ArtifactRequest();
-        request.setArtifact(localArtifact);
-        request.setRepositories(remoteRepos);
+
+        final String version = getActualVersion(artifact, remoteRepos, repoSystem, repoSession);
+
+        final ArtifactRequest request = new ArtifactRequest(
+                new DefaultArtifact(
+                        artifact.getGroupId(),
+                        artifact.getArtifactId(),
+                        artifact.getClassifier(),
+                        artifact.getExtension(),
+                        version),
+                remoteRepos,
+                null
+        );
 
         try {
             final ArtifactResult result = repoSystem.resolveArtifact(repoSession, request);
             return result.getArtifact();
         } catch (ArtifactResolutionException e) {
-            throw new RuntimeException("Cannot resolve artifact '" + localArtifact.toString() + "'", e);
+            throw new RuntimeException("Cannot resolve artifact '" + artifact.toString() + "'", e);
+        }
+    }
+
+    private static String getActualVersion(final Artifact artifact, List<RemoteRepository> remoteRepos, RepositorySystem repoSystem, RepositorySystemSession repoSession) throws RuntimeException {
+        try {
+            //Place classifier & extension in correct order
+            final String artifactTail = artifact.getClassifier() + "." + artifact.getExtension();
+            final VersionRequest vr = new VersionRequest(
+                    new DefaultArtifact(
+                            artifact.getGroupId(),
+                            artifact.getArtifactId(),
+                            artifactTail.replaceAll("^([^\\.]+).*", "$1"),
+                            artifactTail.replaceAll("^[^\\.]+\\.", ""),
+                            artifact.getVersion()),
+                    remoteRepos,
+                    null
+            );
+
+            return repoSystem.resolveVersion(repoSession, vr).getVersion();
+        } catch (VersionResolutionException e) {
+            throw new RuntimeException("Cannot resolve artifact version '" + artifact.toString() + "'", e);
         }
     }
 
